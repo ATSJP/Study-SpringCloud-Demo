d30a5c65d3d8aa6bbceb11a24cc47c0ac9130311
shijianpeng <shijianpeng@xyz.cn>
2021-12-13 14:06:28 +0800

    Eureka-Server

---
+++ Eureka/Base/Eureka-B/src/test/java/com/example/eurekab/EurekaBApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.eurekab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaBApplicationTests {
+
+  @Test
+  void contextLoads() {}
+
+}
